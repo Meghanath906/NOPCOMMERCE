fc00af4dc5daef1315966fdd873d87da3dc144ad
Meghanath906 <Meghanath906@gmail.com>
2022-10-18 21:50:51 +0000

    Revert "this is my First commit...."
    This reverts commit 47ca8395d859fc2524c2299bdeb257cad020c520.

--- run.bat
+++
@@ -1,7 +0,0 @@
-pytest -s -v -m "sanity" --html=./Reports/reports.html testCases/ --browser chrome
-
-rem pytest -s -v -m "sanity or regression" --html=./Reports/reports.html testCases/ --browser chrome
-
-rem pytest -s -v -m "sanity and regression" --html=./Reports/reports.html testCases/ --browser chrome
-
-rem pytest -s -v -m "regression" --html=./Reports/reports.html testCases/ --browser chrome
